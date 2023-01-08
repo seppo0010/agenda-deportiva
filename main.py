476d4ea6a9821f9d432712bf69af75104b6b71fc
Sebastian Waisbrot <seppo0010@gmail.com>
2023-01-08 01:06:57 -0300

    Add html preview

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import os
 import shutil
 import html
 from datetime import timedelta
+import dateutil.parser
 from ics import Calendar, Event
 
 OUT_DIR = os.environ.get('OUT_DIR', './out')
@@ -32,7 +33,21 @@ for torneo, c in torneos.items():
         f.writelines(c.serialize_iter())
 
 with open(f'{OUT_DIR}/index.html', 'w') as f:
-    f.write('<ul>\n')
-    for torneo in torneos.keys():
-        f.write(f'<li><a href="{html.escape(torneo)}.ics" target="_blank">{html.escape(torneo)}</a></li>\n')
-    f.write('</ul>\n')
+    f.write('<!DOCTYPE html>\n')
+    f.write('<html lang="es">\n')
+    f.write('<head><title>Agenda Deportiva</title></head>\n')
+    f.write('<body>\n')
+    f.write('<table>\n')
+    for torneo, cal in torneos.items():
+        f.write('<tr>\n')
+        f.write(f'<th colspan="3"><a href="{html.escape(torneo)}.ics" target="_blank">{html.escape(torneo)}</a></th>\n')
+        f.write('</tr>\n')
+        for ev in sorted(cal.events, key=lambda ev: str(ev.begin)):
+            f.write('<tr>\n')
+            f.write(f'<td>{ev.name}</td>\n')
+            f.write(f'<td>{dateutil.parser.isoparse(str(ev.begin)).strftime("%d/%m/%y %H:%M")}</td>\n')
+            f.write(f'<td>{ev.description}</td>\n')
+            f.write('</tr>\n')
+    f.write('</table>\n')
+    f.write('</body>\n')
+    f.write('</html>\n')
